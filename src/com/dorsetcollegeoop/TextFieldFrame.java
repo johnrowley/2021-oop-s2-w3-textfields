40c8e48befae56248c983541a7da61e7f6dc8d60
John Rowley <outsidethebox@eircom.net>
2021-02-09 15:39:24 +0000

    added event listeners

--- src/com/dorsetcollegeoop/TextFieldFrame.java
+++ src/com/dorsetcollegeoop/TextFieldFrame.java
@@ -19,20 +19,50 @@ public class TextFieldFrame extends JFrame {
 
         textField1 = new JTextField(10);
         add(textField1);
-        textField2 = null;
-        textField3 = null;
-        passwordField = null;
+        textField2 = new JTextField("Enter Text here");
+        add(textField2);
+        textField3 = new JTextField("Cannot be edited", 21);
+        textField3.setEditable(false);
+        add(textField3);
+        passwordField = new JPasswordField("Hidden Text");
+        add(passwordField);
 
         TextFieldHandler handler = new TextFieldHandler();
         textField1.addActionListener(handler);
+        textField2.addActionListener(handler);
+        textField3.addActionListener(handler);
+        passwordField.addActionListener(handler);
+
 
     }
 
     private class TextFieldHandler implements ActionListener {
 
-      public void actionPerformed(ActionEvent event) {
-        String message = "Not yet handled";
-          JOptionPane.showMessageDialog(null,message);
+        public void actionPerformed(ActionEvent event) {
+            String message = "Not yet handled";
+
+            if (event.getSource() == textField1) {
+                message = String.format("textfield1: %s", event.getActionCommand());
+
+            }
+
+            else if (event.getSource() == textField2) {
+                message = String.format("textfield2: %s", event.getActionCommand());
+
+            }
+
+            else if (event.getSource() == textField3) {
+                message = String.format("textfield3: %s", event.getActionCommand());
+
+            }
+
+            else if (event.getSource() == passwordField) {
+                message = String.format("passwordField: %s", event.getActionCommand());
+
+            }
+
+
+            JOptionPane.showMessageDialog(null, message);
         }
     }
 
